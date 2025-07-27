8e3432e391a8eee27ffcc8f60618558082f4bd75
Tharanafernando <tharana.611@gmail.com>
2025-07-27 09:56:54 +0530

    Connect frontend and backend

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import express from 'express'
 import {databaseConnection} from "./DataBase/dbConnection.js";
 import dotenv from "dotenv";
 import categoryRouter from "./API/Category.js";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 
 const port = 8000;
+app.use(cors());
 app.use(express.json());
 app.use('/api/products',productRouter)
 app.use('/api/category',categoryRouter)

287b6ff23783d268345cc95a4e0dba9fbb244db1
Tharanafernando <tharana.611@gmail.com>
2025-07-27 10:00:53 +0530

    modified

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ import productRouter from "./API/Products.js";
 import express from 'express'
 import {databaseConnection} from "./DataBase/dbConnection.js";
 import dotenv from "dotenv";
-import categoryRouter from "./API/Category.js";
 import cors from "cors";
-
 dotenv.config();
 
 const app = express();
 
 const port = 8000;
-app.use(cors());
 app.use(express.json());
+app.use((cors()));
 app.use('/api/products',productRouter)
 await databaseConnection()
 

12b5653e70726966528af9029e4bdc30bc4f8ca3
Tharanafernando <tharana.611@gmail.com>
2025-07-27 10:12:51 +0530

    add category

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import productRouter from "./API/Products.js";
+import categoryRouter from "./API/Category.js";
 import express from 'express'
 import {databaseConnection} from "./DataBase/dbConnection.js";
 import dotenv from "dotenv";
@@ -9,6 +10,7 @@ const port = 8000;
 app.use(express.json());
 app.use((cors()));
 app.use('/api/products',productRouter)
+app.use('/api/category',categoryRouter)
 await databaseConnection()
 
 app.listen(port, () => {
